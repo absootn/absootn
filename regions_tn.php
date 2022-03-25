24e1adbed72d9ada95fc2164b68ac8b93139bec2
absootn <93208542+absootn@users.noreply.github.com>
2022-03-25 17:37:18 +0100

    Create regions_tn.php

---
+++ regions_tn.php
@@ -0,0 +1,41 @@
+// Mondher ABBASSI Activités informatique @absootn
+// Multilanguage Tunisia regions in php array
+
+$regions = array (
+array(
+id => "1",
+ar => "الشمال الشرقي",
+en => "North east",
+fr => "Nord est"
+),
+array(
+id => "2",
+ar => "الوسط الشرقي",
+en => "Center east",
+fr => "Centre est"
+),
+array(
+id => "3",
+ar => "الجنوب الشرقي",
+en => "South east",
+fr => "Sud est"
+),
+array(
+id => "4",
+ar => "الشمال الغربي",
+en => "North west",
+fr => "Nord ouest"
+),
+array(
+id => "5",
+ar => "الوسط الغربي",
+en => "Center west",
+fr => "Centre ouest"
+),
+array(
+id => "6",
+ar => "الجنوب الغربي",
+en => "South west",
+fr => "Sud ouest"
+)
+);
